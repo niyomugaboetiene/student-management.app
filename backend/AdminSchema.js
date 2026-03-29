e80f571d0bb7b5cb914a59fb0d7760f27cae95b6
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-03-29 17:56:13 +0200

    [Coding] implementing backend logic of my app

---
+++ backend/AdminSchema.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import AutoIncrementFactory from "mongoose-sequence"
+
+const AutoIncrement = AutoIncrementFactory(mongoose);
+
+
+const AdminSchema = mongoose.Schema({
+    admin_id: { required: true, unique: true, type: Number },
+    full_name: { required: true, type: String },
+    email: { required: true, unique: true, type: String },
+    phone: { required: true, unique: true, type: String },
+    location: { required: true, unique: true, type: String },
+    joined_at: {type: Date, default: Date.now() },
+});
+
+StudentSchema.plugin(AutoIncrement, { inc_field: 'student_id'});
+export default mongoose.model("student", StudentSchema);
