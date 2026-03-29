60e81e4067e391b7ba15b1a605acf2663a48b93a
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-03-29 17:46:54 +0200

    [Coding] implementing backend logic of my app

--- backend/StudentSchema.js
+++ backend/StudentSchema.js
@@ -5,5 +5,13 @@ const AutoIncrement = AutoIncrementFactory(mongoose);
 
 
 const StudentSchema = mongoose.Schema({
-    
-})
+    student_id: { required: true, unique: true, type: Number },
+    full_name: { required: true, unique: true, type: String },
+    roll: { required: true, unique: true, type: Number, ref: "class" },
+    email: { required: true, unique: true, type: String },
+    course: { required: true, unique: true, type: Number, ref: "course" },
+    phone: { required: true, unique: true, type: String }
+});
+
+StudentSchema.plugin(AutoIncrement, { inc_field: 'student_id'});
+export default mongoose.model("student", StudentSchema);
