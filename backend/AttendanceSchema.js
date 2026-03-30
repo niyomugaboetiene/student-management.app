ef901213c488cc83a3f70fc6d180fb3aad780cf1
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-03-30 07:06:58 +0200

    [Coding] Create schema of each collection

---
+++ backend/AttendanceSchema.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import AutoIncrementFactory from "mongoose-sequence"
+
+const AutoIncrement = AutoIncrementFactory(mongoose);
+
+const AttendanceSchema = mongoose.Schema({
+    attendance_id: { required: true, unique: true, type: Number },
+    student: { required: true, type: mongoose.Schema.Types.ObjectId, ref: "student" },
+    class: { type: mongoose.Schema.Types.ObjectId, required: true },
+    course: { required: true, type: String },
+    date: { type: Date, required: true },
+    status: { type: String, enum: ["present", "absent"], default: "absent"}
+});
+
+AttendanceSchema.plugin(AutoIncrement, { inc_field: 'department_id' });
+export default mongoose.model("teacher", AttendanceSchema);
