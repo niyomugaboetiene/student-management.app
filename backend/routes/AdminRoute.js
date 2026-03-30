1cc44dd209f46d3bdee5cd1753f5b4b34d6a9e52
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-03-30 07:52:19 +0200

    [Coding] Create schema of each collection

--- backend/routes/AdminRoute.js
+++ backend/routes/AdminRoute.js
@@ -1,4 +1,14 @@
 import connection from "../db/connection.js";
-
+import express from "express";
+import mongoose from "mongoose";
 connection();
 
+const router = express();
+
+router.post('/admin/add', async (req, res) => {
+   try {
+    // admin_id,  full_name, email, phone, location, joined_at, password, last_login
+    const { admin_id, full_name, email, phone, location, password } = req.body;
+    
+   }
+});
