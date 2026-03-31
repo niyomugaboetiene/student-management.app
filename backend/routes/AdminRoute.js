c6a99a8f1c378f69cdef2f5f326d7c0e8da0e0b1
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-03-31 17:04:15 +0200

    [Coding] implementing admin route

--- backend/routes/AdminRoute.js
+++ backend/routes/AdminRoute.js
@@ -55,7 +55,7 @@ router.get('/:_id', async (req, res) => {
 router.put('/update/:_id', async ( req, res) => {
     try {
         const { full_name, email, phone, location, password } = req.body;
-        const { _id } = req.params;
+        const { _id } = req.params._id;
 
         const adminUsers = await AdminSchema.findOne({_id: _id});
 
