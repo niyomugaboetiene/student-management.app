c2bbb4898a100cdb07c2216ce2cfc4efc239fbdb
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-03-31 17:36:31 +0200

    [Coding] implementing admin route

---
+++ backend/routes/StudentRoute.js
@@ -0,0 +1,36 @@
+import StudentSchema from "../schema/StudentSchema.js";
+import express from "express";
+import bcrypt from "bcrypt";
+
+const router = express();
+
+router.post('/register', async (req, res) => {
+    //     student_id
+        // full_name,  gender, roll, email, trade, phone, location, class, password, last_login, is_approved
+
+        try {
+            const { full_name, gender, email, roll, trade, phone, location, classe, password } = req.body;
+
+            if (!full_name || !gender || !email || !roll || !trade || !phone || !location || !classe || !password) {
+                 return res.status(404).json({ message: 'Fill some missing fields' }); 
+            }
+
+            const salt = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(password, 10);
+
+            await StudentSchema.create({
+                full_name,
+                gender,
+                email,
+                roll,
+                trade,
+                phone,
+                location,
+                class: classe,
+                password: hashedPassword
+            });
+        } catch (err) {
+            console.error(err);
+            return res.status(500).json({ message: 'Server error'});
+        }
+})
