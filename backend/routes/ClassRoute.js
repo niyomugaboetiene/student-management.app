0339066ab9caf513417eaeee9f7776adff190b0a
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-04-01 17:31:12 +0200

    [Coding] implementing  route

--- backend/routes/ClassRoute.js
+++ backend/routes/ClassRoute.js
@@ -32,6 +32,28 @@
                 console.error(err);
                 return res.status(500).json({ message: 'Server error'});
             }
-    })
+    });
+
+router.get("/:id", async (req, res) => {
+    try {
+        const { _id } = req.params;
+
+        if (!_id) {
+            return res.status(403).json({ message: 'Id required' });
+        }
+
+        const ClassList = await ClassSchema.findOne(_id);
+
+        if (!ClassList) {
+            return res.status(404).json({ message: 'Id does not exist' });
+        }
+
+        return res.status(200).json({ message: 'Classes', classes: ClassList });
+
+    } catch (err) {
+          console.error(err);
+          return res.status(500).json({ message: 'Server error'});
+    }
+})
     
     export default router;
