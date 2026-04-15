03b79327ae207b1e84738aa3d72e1a3bad61a078
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-04-15 16:33:23 +0200

    [coding] implementing attendance route

--- backend/routes/AttendanceRoute.js
+++ backend/routes/AttendanceRoute.js
@@ -1,5 +1,6 @@
 import express from "express";
 import AttendanceSchema from "../schema/AttendanceSchema.js";
+import StudentSchema from "../schema/StudentSchema.js";
 
 const router = express.Router();
 
@@ -56,6 +57,43 @@ router.get('/', async (req, res) => {
         const start = new Date(year, month - 1, day, 0, 0, 0);
         const end = new Date(year, month - 1, day, 23, 59, 59);
 
+        const attendance = await AttendanceSchema.find({
+            class_id,
+            date: { $gte: start, $lte: end }
+        });
+
+        if (attendance.length === 0) {
+            return res.status(404).json({ message: 'No attendance done on this day' });
+        }
+
+        return res.status(200).json({
+            message: 'Attendance details',
+            attendances: attendance
+        });
+
+
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ message: 'Internal server error' });
+    }
+});
+
+// get attendance by date/class/student
+router.get('/', async (req, res) => {
+    try {
+        const { day, month, student_id } = req.query;
+        
+        const year = new Date().getFullYear();
+        const start = new Date(year, month - 1, day, 0, 0, 0);
+        const end = new Date(year, month - 1, day, 23, 59, 59);
+
+        const student_details = await StudentSchema.findById({_id: student_id});
+
+        if (class_of_student.length === 0) {
+            return res.status(404).json({ message: 'NO student found for this ID' });
+        }
+
+        const class_of_student = student_details.class;
         const attendance = await AttendanceSchema.find({
             class_id,
             date: { $gte: start, $lte: end }
