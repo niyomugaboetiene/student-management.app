3f406962e04c2d486032fad31d6541792c5427db
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-04-16 10:00:24 +0200

    [coding] implemeng admin auth frontend

---
+++ frontend/src/features/auth/admin/Login.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { useState } from "react";
+import axios from "axios";
+
+
+const AdminLogin = () => {
+    const [full_name, setFull_name] = useState("");
+    const [phone, setPhone] = useState("");
+    const [passsword, setPassword] = useState("");
+    const [message, setMessage] = useState("");
+    const [error, setError] = useState("");
+    const [loading, setLoading] = useState(false);
+
+    const handleLoginUser = async () => {
+        try {
+            setLoading(true);
+            const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+            const res = await axios.post(`${BACKEND_URL}/admin/auth/login`, {
+                full_name,
+                phone,
+                passsword
+            }, { withCredentials: true });
+ 
+          setLoading(false);
+          setMessage(res.data.message);
+        } catch (err) {
+            setLoading(false);
+            console.error(err);
+            const errorMessage = err?.response.error;
+            setError(errorMessage);
+        }
+    }
+}
