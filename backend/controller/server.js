157cb0c28421e84a5cbbe7a1c1598cbf8cbe638c
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-04-16 10:10:16 +0200

    implementing admin auth

--- backend/controller/server.js
+++ backend/controller/server.js
@@ -28,7 +28,8 @@ connection();
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin: '*'
+    origin: 'http://localhost:5173',
+    credentials: true
 }));
 
 app.use(session({
