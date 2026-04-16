982b8a8b2b8bb47a30acff88d4e681ac03ecbf84
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-04-16 10:03:55 +0200

    [coding] implemeng admin auth frontend

--- frontend/src/features/auth/admin/Login.jsx
+++ frontend/src/features/auth/admin/Login.jsx
@@ -35,11 +35,32 @@ const AdminLogin = () => {
     return (
         <div>
             <div>
+
+                {message && (
+                    <p>{message}</p>
+                )}
+
+                {error && (
+                    <p>{error}</p>
+                )}
                 <h1>Login Admin Portal</h1>
 
                 <div>
-                    
+                    <label htmlFor="">Full name</label>
+                    <input type="text"  onChange={(e) => setFull_name(e.target.value)}/>
                 </div>
+                
+                <div>
+                    <label htmlFor="">Phone</label>
+                    <input type="text"  onChange={(e) => setPhone(e.target.value)}/>
+                </div>
+                
+                <div>
+                    <label htmlFor="">Password</label>
+                    <input type="password"  onChange={(e) => setPassword(e.target.value)}/>
+                </div>
+
+                <button onClick={handleLoginUser}>Login</button>
             </div>
         </div>
     )
