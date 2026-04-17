d46c9a34f295d944f6ce1b7513490cb3bb949165
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-04-17 07:16:17 +0200

    Login admin login page protal

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import AdminLogin from './features/auth/admin/Login'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 function App() {
 
   return (
-    <>
-         <AdminLogin />
-    </>
+       <BrowserRouter>
+           <Routes>
+               <Route path='/admin/login' element={<AdminLogin />}/>
+           </Routes>
+       </BrowserRouter>
   )
 }
 
