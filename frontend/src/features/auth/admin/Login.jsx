afdf8def5d11780c3b8f7ff4d0ee22889c47ab0c
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-04-17 06:47:22 +0200

    Login admin login page protal

--- frontend/src/features/auth/admin/Login.jsx
+++ frontend/src/features/auth/admin/Login.jsx
@@ -41,7 +41,7 @@ const AdminLogin = () => {
         <div className="bg-gray-100 min-h-screen flex justify-center items-center p-3">
             <div className="bg-white w-110 rounded-xl p-4 ">
                 {message && (
-                    <div className="bg-red-500 mb-2 p-2 rounded-lg text-white font-bold">
+                    <div className="bg-red-500 mb-2 p-2 rounded-lg text-white font-bold relative">
                         <p>{message}</p>
                     </div>
                 )}
