b3e8ee2cc4fb6829a879201df8ed9ecedd394051
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-04-17 06:19:04 +0200

    Login page

--- frontend/src/features/auth/admin/Login.jsx
+++ frontend/src/features/auth/admin/Login.jsx
@@ -47,17 +47,17 @@ const AdminLogin = () => {
 
                 <div>
                     <label htmlFor="">Full name</label>
-                    <input type="text"  onChange={(e) => setFull_name(e.target.value)}/>
+                    <input type="text"  onChange={(e) => setFull_name(e.target.value)} required/>
                 </div>
                 
                 <div>
                     <label htmlFor="">Phone</label>
-                    <input type="text"  onChange={(e) => setPhone(e.target.value)}/>
+                    <input type="text"  onChange={(e) => setPhone(e.target.value)} required/>
                 </div>
                 
                 <div>
                     <label htmlFor="">Password</label>
-                    <input type="password"  onChange={(e) => setPassword(e.target.value)}/>
+                    <input type="password"  onChange={(e) => setPassword(e.target.value)} required/>
                 </div>
 
                 <button onClick={handleLoginUser}>Login</button>
