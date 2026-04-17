6c6d5a414b175da885f9ce9c4ee580fe079b35d0
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-04-17 06:26:31 +0200

    Login admin login page protal

--- frontend/src/features/auth/admin/Login.jsx
+++ frontend/src/features/auth/admin/Login.jsx
@@ -13,7 +13,11 @@ const AdminLogin = () => {
 
     const handleLoginUser = async () => {
         try {
-            console.log("Received data", full_name, phone, password);
+            // console.log("Received data", full_name, phone, password);
+            if (!full_name || !phone || !password) {
+                setMessage("Fill out some missing fields");
+                return;
+            }
             setLoading(true);
             const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
             const res = await axios.post(`${BACKEND_URL}/admin/auth/login`, {
