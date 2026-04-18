e444a3b836a043cae72d97e3126b25df5cf37fc8
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-04-18 08:44:24 +0200

    implementing dashboard frontend

---
+++ frontend/src/features/dashboard/Dashborad.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+const DashboardPage = () => {
+
+    return (
+        <div>
+            <div>
+                <nav>
+                    <Link>Home</Link>
+                    <Link>Admin panel</Link>
+                    <Link>Marks</Link>
+                    <Link>View Marks</Link>
+                    <Link>Attenndance</Link>
+                    <Link>Attenndance</Link>
+                </nav>
+            </div>
+        </div>
+    )
+}
