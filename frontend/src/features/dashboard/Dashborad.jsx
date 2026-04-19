ed260ef95a4303ddebddb9932ccb67335dc94c53
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-04-19 08:46:19 +0200

    implementing dashboard frontend

--- frontend/src/features/dashboard/Dashborad.jsx
+++ frontend/src/features/dashboard/Dashborad.jsx
@@ -5,6 +5,7 @@ const DashboardPage = () => {
 
     return (
         <div className="bg-gray-100 min-h-screen flex">
+            <div>
             <div className="flex items-center justify-center  bg-cyan-400 h-14 w-full p-4">
                 <nav className="flex space-x-5 text-gray-900 text-md">
                     <Link>Dashboard</Link>
@@ -18,7 +19,7 @@ const DashboardPage = () => {
                     <Link>Trade</Link>
                     <Link>Marks</Link>
                 </nav>
-
+            </div>
             </div>
         </div>
     )
