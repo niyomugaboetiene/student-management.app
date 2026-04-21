fc7f20fb220dde17514fb272186fa96125259fac
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-04-21 15:06:31 +0200

    implementing department pages frontend

--- frontend/src/features/department/UpdateDepartment.jsx
+++ frontend/src/features/department/UpdateDepartment.jsx
@@ -34,10 +34,14 @@ const UpdateDepartment = () => {
     const GetExistingDepartment = async () => {
           try {
             setIsLoading(true);
-            const trRes = await axios.get(`${BACKEND_URL}/teacher/${_id}`, { withCredentials: true });
-            console.log(trRes.data.department);
+            const trRes = await axios.get(`${BACKEND_URL}/department/${_id}`, { withCredentials: true });
+            console.log("Dep", trRes.data.department);
             const values = trRes.data.department;
-            setName(values.name)
+
+            setName(values.name || "");
+            setBuilding(values.building || "");
+            setDescription(values?.description || "");
+            setHOD(values.HOD);
             setIsLoading(false);
           } catch (err) {
             console.error(err);
@@ -47,7 +51,7 @@ const UpdateDepartment = () => {
 
     useEffect(() => {
       GetExistingDepartment();
-    }, [_id]);
+    }, []);
 
     useEffect(() => {
         handleGetTeacher();
@@ -60,7 +64,7 @@ const UpdateDepartment = () => {
                 setError("Fill out the missing fields");
                 setMessage("");
             }
-            const res = await axios.post(`${BACKEND_URL}/department/update/${_id}`, { name, description, building, HOD }, { withCredentials: true });
+            const res = await axios.put(`${BACKEND_URL}/department/update/${_id}`, { name, description, building, HOD }, { withCredentials: true });
             setMessage(res.data.message);
             setIsLoading(false);
         } catch (err) {
@@ -96,7 +100,8 @@ const UpdateDepartment = () => {
                 <h1 className="text-xl text-gray-600 font-bold">Add Department Portal</h1>
 
                 <div className="mt-3">
-                    <input type="text"  
+                    <input type="text"
+                       value={name}  
                        onChange={(e) => setName(e.target.value)} required
                        className="bg-gray-100  w-full p-3 rounded-full focus:outline-1 focus:outline-gray-500" placeholder="Dep name"
                     />
@@ -104,6 +109,7 @@ const UpdateDepartment = () => {
                 
                 <div className="mt-3">
                     <input type="text"  
+                       value={description}
                        onChange={(e) => setDescription(e.target.value)}
                        className="bg-gray-100  w-full p-3 rounded-full focus:outline-1 focus:outline-gray-500" placeholder="Description (Optional)"
                     />
@@ -111,6 +117,7 @@ const UpdateDepartment = () => {
                 
                 <div className="mt-3">
                     <input type="text"  
+                    value={building}
                     onChange={(e) => setBuilding(e.target.value)} required
                     className="bg-gray-100 w-full p-3 rounded-full focus:outline-1 focus:outline-gray-500" placeholder="Building"
                 />
@@ -118,6 +125,7 @@ const UpdateDepartment = () => {
                 
                 <div className="mt-3 mb-4">
                     <select  
+                       value={HOD}
                        onChange={(e) => setHOD(e.target.value)} 
                        className="bg-gray-100 w-full rounded-full p-3 focus:outline-1 focus:outline-gray-500"
                     > 
