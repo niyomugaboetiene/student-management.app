43aa0cca23c06c6223bcb47641ee9d8a35f72611
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-04-21 11:40:26 +0200

    implementing trade frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,18 @@
 import { useState } from 'react'
 import AdminLogin from './features/auth/admin/Login'
+import AdminRegister from './features/auth/admin/Register'
+import StudentLogin from './features/auth/student/Login'
+import StudentRegister from './features/auth/student/Register'
+import TeacherRegister from './features/auth/teacher/Register'
+import TeacherLogin from './features/auth/teacher/Login'
+
+import DashboardPage from './features/dashboard/Dashborad';
+
+// trade protal
+import AddTrade from './features/trade/AddTrade'
+import GetTradeList from "./features/trade/TradeList";
+import UpdateTrade from "./features/trade/UpdateTrade";
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 function App() {
 
@@ -7,6 +20,21 @@ function App() {
        <BrowserRouter>
            <Routes>
                <Route path='/admin/login' element={<AdminLogin />}/>
+               <Route path='/admin/register' element={<AdminRegister />}/>
+
+
+               <Route path='/student/login' element={<StudentLogin />}/>
+               <Route path='/student/register' element={<StudentRegister />}/>
+
+               <Route path='/teacher/register' element={<TeacherRegister />}/>
+               <Route path='/teacher/login' element={<TeacherLogin />}/>
+
+               <Route path='/' element={<DashboardPage />}/>
+
+               {/* Trade routes */}
+               <Route path='/trade/add' element={<AddTrade />} />
+               <Route path='/trade/list' element={<GetTradeList />} />
+               <Route path='/trade/update/:_id' element={<UpdateTrade />} />
            </Routes>
        </BrowserRouter>
   )
