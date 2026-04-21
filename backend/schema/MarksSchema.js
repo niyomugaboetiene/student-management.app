d20a24aa258b147352ff5aeaec6f246762cfad52
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-04-21 15:34:26 +0200

    implementing marks schema

--- backend/schema/MarksSchema.js
+++ backend/schema/MarksSchema.js
@@ -8,7 +8,7 @@ const MarkSchema = mongoose.Schema({
     marks_id: { unique: true, type: Number },
     student: { required: true, type: mongoose.Schema.Types.ObjectId, ref: "students" },
     class: { required: true, type: mongoose.Schema.Types.ObjectId, ref: "classes" },
-    subject: { required: true, type: String },
+    subject: { required: true, type: mongoose.Schema.Types.ObjectId, ref: "subjects" },
     marks: { required: true, type: Number },
 }, { timestamps: true });
 
