7ddc479407bb526c9748119e34f3776328ad714a
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-04-29 08:25:14 +0200

    Implementing attendance pages

--- frontend/src/features/attendance/AttendanceById.jsx
+++ frontend/src/features/attendance/AttendanceById.jsx
@@ -1,27 +1,33 @@
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const HandleGetStudentAttendance = () => {
     const [attendance, setAttendance] = useState([]);
     const [error, setError] = useState("");
     const [loading, setLoading] = useState(false);
+    const { student_id } = useParams();
 
     const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
     const handleGetAttendance = async() => {
         try {
             setLoading(true);
-            const res = await axios.get(`${BACKEND_URL}/attendance/attendanceList`, { withCredentials: true });
+            const res = await axios.get(`${BACKEND_URL}/attendance/stud/${student_id}`, { withCredentials: true });
             setAttendance(res.data.attendance);
             setLoading(false);
         } catch (err) {
             console.error("Error", err);
             const errorMessage = err.response?.data?.message;
-            if (errorMessage === "No attendance in the system. try to add some") {
-                setError("No attendance in the system");
+            if (errorMessage === "Enter a valid student id") {
+                setError(errorMessage);
             }
 
             if (errorMessage === "Internal server error") {
                 setError("Internal server error");
+            }
+            
+            if (errorMessage === "NO attendance for this student") {
+                setError(errorMessage);
             }
 
             setLoading(false);
