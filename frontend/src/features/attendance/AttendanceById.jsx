86408f08f92d9ab303b308c7c378866f2c3c355a
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-04-30 07:35:34 +0200

    Implementing attendance pages

--- frontend/src/features/attendance/AttendanceById.jsx
+++ frontend/src/features/attendance/AttendanceById.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import axios from "axios";
 
 const HandleGetStudentAttendance = () => {
     const [attendance, setAttendance] = useState([]);
