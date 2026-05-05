ce6f33cecbef3988e79c77e61ced4c219b03d83b
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-05-05 15:01:00 +0200

    Implementing student monly report frotnend

---
+++ frontend/src/features/dashboard/Nav.jsx
@@ -0,0 +1,88 @@
+import { useNavigate } from "react-router-dom";
+
+const Nav = () => {
+    const [isNameClicked, setIsNameClicked] = useState(false);
+
+  const navigate = useNavigate();
+
+  const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+
+    const FetchUserSession = async () => {
+       try {
+
+         setIsLoading(true);  
+          const userRes = await axios.get(`${BACKEND_URL}/teacher/auth/userSession`, { withCredentials: true });
+          console.log("Session", userRes.data.user);
+          setUser(userRes.data.user);
+          setIsLoading(false);
+       } catch (err) {
+         console.error(err);
+         setIsLoading(false);
+    }
+}
+
+    const handleLogout = async () => {
+       try {
+         setIsLoading(true);  
+          await axios.post(`${BACKEND_URL}/teacher/auth/logout`, {}, { withCredentials: true });
+          setUser(null);
+          navigate('/admin/login');
+          setIsLoading(false);
+       } catch (err) {
+         console.error(err);
+         setIsLoading(false);
+    }
+}
+
+    useEffect(() => {
+        FetchUserSession();
+    }, []);
+
+    return (
+        <div className="flex justify-between bg-cyan-400 w-full">
+                {isLoading && (
+                    <div className="flex justify-center items-center">
+                        <div className="h-8 w-8 text-cyan-500 border-transparent animate-spin"></div>
+                    </div>
+                )}
+               <div>
+                   <p>Logo</p>
+               </div>
+                <nav className="flex items-center justify-center h-14 w-full font-normal space-x-5 text-gray-900 text-md">
+                    <Link>Dashboard</Link>
+                    <Link>Students</Link>
+                    <Link>Class</Link>
+                    <Link>Teacher</Link>
+                    <Link>Department</Link>
+                    <Link>Admins</Link>
+                    <Link>Attendance</Link>
+                    <Link>Subject</Link>
+                    <Link>Trade</Link>
+                    <Link>Marks</Link>
+                </nav>
+
+                {user && (
+                     <div onClick={() => setIsNameClicked(!isNameClicked)} className="font-bold">
+                         {user.full_name}
+                     </div> 
+                )}
+      
+             {isNameClicked && (
+                <div className="absolute right-2 top-14 bg-cyan-200 p-2 rounded-lg shadow-lg">
+                     <p>Name: <span className="text-gray-900 font-bold">{user.email ? user.email : `no name`}</span></p>
+                     <p>Role: <span className="text-gray-900 font-bold">{user.role ? user.role : `no role`}</span></p>
+                     <p>Phone: <span className="text-gray-900 font-bold">{user.phone ? user.phone : `no phone`}</span></p>
+                     <p>Location: <span className="text-gray-900 font-bold">{user.location ? user.location : `no location`}</span></p>
+
+                  <div className="justify-between flex">
+                        <button className="mt-2 bg-cyan-400 px-5 py-1 rounded-lg text-gray-900 hover:bg-cyan-500">More</button>
+                        <button className="mt-2 bg-red-400 px-5 py-1 rounded-lg text-gray-900 hover:bg-red-500" onClick={handleLogout}>logout</button>
+                  </div>
+                </div> 
+             )}
+
+            </div>
+    )
+}
+
+export default Nav
